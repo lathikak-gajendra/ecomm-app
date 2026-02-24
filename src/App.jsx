30476cd8cda644e3417a1b904504f72bab62a28c
lathikak-gajendra <lathikakgajendra@gmail.com>
2026-02-24 16:47:50 +0530

    removed Product import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Components/Home"
 import "./Components/styles.css"
-import Product from "./Components/product"
+
 import Login from "./Components/Login"
 import Cart from "./Components/Cart"
 import Product1 from "./Components/Product1"
@@ -13,7 +13,7 @@ function App(){
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      {/* <Route path="/products" element={<Product/>}/> */}
+    
       <Route path="/product1" element={<Product1/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/Cart" element={<Cart/>}/>
